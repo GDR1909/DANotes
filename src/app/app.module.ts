64cf3ecee4bc9c15b9e3c1f84d12d842f6784600
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-01-24 21:46:33 +0100

    implented AngularFire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { NoteListComponent } from './note-list/note-list.component';
 import { NoteComponent } from './note-list/note/note.component';
 import { FormsModule } from '@angular/forms';
 import { AddNoteDialogComponent } from './add-note-dialog/add-note-dialog.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 
 @NgModule({
@@ -23,6 +25,8 @@ import { AddNoteDialogComponent } from './add-note-dialog/add-note-dialog.compon
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"danotes-434fa","appId":"1:1025836826191:web:f2e80da33f7e8d9bf1f43a","storageBucket":"danotes-434fa.appspot.com","apiKey":"AIzaSyD4k8Vg5N7qgcNlFhFULRw9pzjDl9Je-Uk","authDomain":"danotes-434fa.firebaseapp.com","messagingSenderId":"1025836826191"})),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
