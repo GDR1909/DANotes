ce03e20db6f88a97741cafe82bd6633247c32c88
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-01-25 20:26:52 +0100

    data access with collection-method and doc-method

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,10 +1,44 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Note } from '../interfaces/note.interface'
+import { Firestore, collection, doc, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
 
-  constructor() { }
+  items$;
+  items;
+
+  firestore: Firestore = inject(Firestore);
+
+
+  constructor() {
+    this.items$ = collectionData(this.getNotesRef());
+    this.items = this.items$.subscribe((list) => {
+      list.forEach(element => {
+        console.log(element);
+      });
+    });
+    this.items.unsubscribe();
+  }
+
+  // const itemCollection = collection(this.firestore, 'items');
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+
+  getSingleDocRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
 }
